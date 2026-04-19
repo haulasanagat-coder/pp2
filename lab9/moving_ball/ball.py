37637528854a37aec13a356c98dea050b124ad4a
Kaula <haulasanagat@gmail.com>
2026-04-19 15:02:45 +0500

    ball

--- lab9/moving_ball/ball.py
+++ lab9/moving_ball/ball.py
@@ -1,19 +1,15 @@
 class Ball:
     def __init__(self, screen_width, screen_height):
-        # 小球初始位置（屏幕中心）
         self.x = screen_width // 2
         self.y = screen_height // 2
         
-        # 大小：半径25 → 50x50
         self.radius = 25
-        self.speed = 20  # 每次移动20像素
+        self.speed = 20 
         
-        # 屏幕边界
         self.max_w = screen_width
         self.max_h = screen_height
 
     def move_up(self):
-        # 不让小球出界
         if self.y - self.speed >= self.radius:
             self.y -= self.speed
 
